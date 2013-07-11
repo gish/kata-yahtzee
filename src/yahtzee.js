7efbabd00812ab5b5943a9ac17134d1bcaa0b3f3
Erik Hedberg <erik@webbgaraget.se>
2013-07-11 10:07:25 +0200

    Wraps Yahtzee declaration in a closure

--- src/yahtzee.js
+++ src/yahtzee.js
@@ -1,72 +1,78 @@
-var Yahtzee = {
-	isPair : function(roll)
-	{
-		var valid = false;
-		var i;
-
-		roll.sort();
-
-		for (i = 0; i < 4; i++)
+(function(root)
+{
+	"use strict";
+	var Yahtzee = {
+		isPair : function(roll)
 		{
-			valid |= (roll[i] === roll[i+1]);
-		}
-		return valid;
-	},
-	isTwoPairs : function(roll)
-	{
-		roll.sort();
-		return	((roll[0] === roll[1]) && (roll[2] === roll[3]) && (roll[0] !== roll[2])) ||
-				((roll[0] === roll[1] || roll[1] === roll[2]) && (roll[3] === roll[4]) && (roll[1] !== roll[3]));
-	},
-	isThreeOfAKind : function(roll)
-	{
-		var valid = false;
-		var i;
+			var valid = false;
+			var i;
 
-		roll.sort();
+			roll.sort();
 
-		for (i = 0; i < 3; i++)
+			for (i = 0; i < 4; i++)
+			{
+				valid |= (roll[i] === roll[i+1]);
+			}
+			return valid;
+		},
+		isTwoPairs : function(roll)
 		{
-			valid |= (roll[i]*1E2 + roll[i + 1]*1E1 + roll[i + 2])%111 === 0;
-		}
-		return valid;
-	},
-	isFourOfAKind : function(roll)
-	{
-		var valid = false;
-		roll.sort();
-
-		valid |= parseInt(roll.slice(0, 4).join(""), 10)%1111 === 0;
-		valid |= parseInt(roll.slice(1, 5).join(""), 10)%1111 === 0;
-
-		return valid;
-	},
-	isSmallStraight : function(roll)
-	{
-		var valid = true;
-		var i;
-
-		roll.sort();
-
-		for (i = 0; i < 5; i++)
+			roll.sort();
+			return	((roll[0] === roll[1]) && (roll[2] === roll[3]) && (roll[0] !== roll[2])) ||
+					((roll[0] === roll[1] || roll[1] === roll[2]) && (roll[3] === roll[4]) && (roll[1] !== roll[3]));
+		},
+		isThreeOfAKind : function(roll)
 		{
-			valid &= (roll[i] === i+1);
-		}
+			var valid = false;
+			var i;
 
-		return valid;
-	},
-	isLargeStraight : function(roll)
-	{
-		var valid = true;
-		var i;
+			roll.sort();
 
-		roll.sort();
-
-		for (i = 0; i < 5; i++)
+			for (i = 0; i < 3; i++)
+			{
+				valid |= (roll[i]*1E2 + roll[i + 1]*1E1 + roll[i + 2])%111 === 0;
+			}
+			return valid;
+		},
+		isFourOfAKind : function(roll)
 		{
-			valid &= (roll[i] === i+2);
-		}
+			var valid = false;
+			roll.sort();
 
-		return valid;
-	}
-};
+			valid |= parseInt(roll.slice(0, 4).join(""), 10)%1111 === 0;
+			valid |= parseInt(roll.slice(1, 5).join(""), 10)%1111 === 0;
+
+			return valid;
+		},
+		isSmallStraight : function(roll)
+		{
+			var valid = true;
+			var i;
+
+			roll.sort();
+
+			for (i = 0; i < 5; i++)
+			{
+				valid &= (roll[i] === i+1);
+			}
+
+			return valid;
+		},
+		isLargeStraight : function(roll)
+		{
+			var valid = true;
+			var i;
+
+			roll.sort();
+
+			for (i = 0; i < 5; i++)
+			{
+				valid &= (roll[i] === i+2);
+			}
+
+			return valid;
+		}
+	};
+
+	root.Yahtzee = root.Yahtzee || Yahtzee;
+}(this));
